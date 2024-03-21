b0a1482b84e1c7192dc6a2d49087e91a0de98d19
Hariharan <109363771+hariFED@users.noreply.github.com>
2024-03-22 03:21:42 +0530

    updated

--- src/component/blog-card.jsx
+++ src/component/blog-card.jsx
@@ -8,20 +8,18 @@ const BlogCard = ({
     title,
     description,
     publishername,
-    publisherpic,
-    banner
 }) => {
     return (
         <div className=" text-[#121212] w-[90%] bg-[#ffffff]  md:w-1/5 flex flex-col  items-center justify-center mx-auto rounded-xl border border-black shadow-[15px_20px_5px_rgba(4,4,3)] ">
-            <div className="p-5 flex flex-col gap-4">
-                <div className=" flex items-center" >
-                    <img className=" rounded-xl" src={banner} alt="illustration" />
+            <div className="flex flex-col gap-4 p-5">
+                <div className="flex items-center " >
+                    <img className=" rounded-xl" src="/src/assets/illustration-article.svg" alt="illustration" />
                 </div>
-                <div className="text-wrap text-left flex items-center">
+                <div className="flex items-center text-left text-wrap">
                     <span className="bg-[#f4d04e] px-[10px] py-1 text-sm font-extrabold rounded-md">{cateagory}</span>
                 </div>
 
-                <div className=" font-semibold">
+                <div className="font-semibold ">
                     Published {date}
                 </div>
                 <div className=" cursor-pointer text-2xl font-bold hover:text-[#f4d04e]">
@@ -30,8 +28,8 @@ const BlogCard = ({
                 <div className="text-[#808080] font-medium">
                     {description}
                 </div>
-                <div className=" cursor-pointer flex items-center mt-2 gap-2">
-                    <img width={40} src={publisherpic} alt="image-avatar" />
+                <div className="flex items-center gap-2 mt-2 cursor-pointer ">
+                    <img width={40} src="/src/assets/illustration-article.svg" alt="image-avatar" />
                     <div className="font-extrabold">{publishername}</div>
                 </div>
             </div>
